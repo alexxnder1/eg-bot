a806b62a5a0ce1fb5f64fd7f16d9e1b853941da0
alexxnder1 <alexxnder.developer@gmail.com>
2022-07-27 21:31:33 +0300

    added procfile for heroku

--- index.js
+++ index.js
@@ -6,7 +6,9 @@ const fs = require('fs');
 
 const eventPath = path.join(__dirname, "events");
 const eventFiles = fs.readdirSync(eventPath).filter(file => file.endsWith('.js'));
+
 require('./db/main');
+
 require('dotenv').config();
 
 const Client = new Discord.Client({
