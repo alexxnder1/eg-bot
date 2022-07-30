8819cbb16144faa433fc0f6340200e20bb337aa9
alexxnder1 <alexxnder.developer@gmail.com>
2022-07-30 13:46:36 +0300

    #1

--- commands/queue.js
+++ commands/queue.js
@@ -1,38 +1,55 @@
 const { distube } = require('../commands/play');
-const Discord = require('discord.js');
 
 module.exports = {
     execute(message, arg, splitted) {
-        if(!distube.getQueue(message))
-            return message.reply('There is not any song that is playing.');
-
-        if(distube.getQueue(message).voiceChannel !== message.member.voice.channel)
-            return message.reply('You are not in the same voice channel as me.');
-
-        var songs = [];
+        let option = splitted[1];
+        switch(option)
+        {
+            case 'clear': {
+                if(!distube.getQueue(message) || !distube.getQueue(message).songs.length)
+                    return message.reply('There is not any song that is playing.');
+      
+                if(distube.getQueue(message).voiceChannel !== message.member.voice.channel)
+                    return message.reply('You are not in the same voice channel as me.');
         
-        const getIndex = (song) => {
-            return distube.getQueue(message).songs.indexOf(song) + 1;
-        }
+                message.reply(`✅ Removed ${distube.getQueue(message).songs.length} songs from queue.`);
+                distube.getQueue(message).songs = [];
+                break;
+            }
 
-        distube.getQueue(message).songs.forEach((song) => {
-            songs.push( { name: `${(getIndex(song) == 1) ? ('`') : ('')} ${getIndex(song)}. ${song.name} ${(getIndex(song) == 1) ? ('🎶 `') : ('')}`, value: `${(getIndex(song) == 1) ? ('`') : ('')}added by ${song.member.user.tag} ${(getIndex(song) == 1) ? ('`') : ('')}`, inline: false } );
-        });
-
-        const embed = {
-            color: 0x4287f5,
-            title: `📜 Queue (${distube.getQueue(message).songs.length})`,
-            description: 'These are currently playing songs:',
-
-            fields: songs,
-
-            footer: {
-                text: 'Eastern Games BOT',
-                icon_url: "https://imgur.com/1nqnLxd.png",
+            default: {
+                if(!distube.getQueue(message))
+                    return message.reply('There is not any song that is playing.');
+        
+                if(distube.getQueue(message).voiceChannel !== message.member.voice.channel)
+                    return message.reply('You are not in the same voice channel as me.');
+        
+                var songs = [];
+                
+                const getIndex = (song) => {
+                    return distube.getQueue(message).songs.indexOf(song) + 1;
+                }
+        
+                distube.getQueue(message).songs.forEach((song) => {
+                    songs.push( { name: `${(getIndex(song) == 1) ? ('`') : ('')} ${getIndex(song)}. ${song.name} ${(getIndex(song) == 1) ? ('🎶 `') : ('')}`, value: `${(getIndex(song) == 1) ? ('`') : ('')}added by ${song.member.user.tag} ${(getIndex(song) == 1) ? ('`') : ('')}`, inline: false } );
+                });
+        
+                const embed = {
+                    color: 0x4287f5,
+                    title: `📜 Queue (${distube.getQueue(message).songs.length})`,
+                    description: 'These are currently playing songs:',
+        
+                    fields: songs,
+        
+                    footer: {
+                        text: 'Eastern Games BOT',
+                        icon_url: "https://imgur.com/1nqnLxd.png",
+                    }
+                }
+        
+                message.channel.send({ embeds: [embed] });
+                break;  
             }
         }
-
-        message.channel.send({ embeds: [embed] });
-
     }
 }
---
+++ todo.md
@@ -0,0 +1,2 @@
+eg! prefix -> slash commands.
+clear command
