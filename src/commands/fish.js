5ccfadfea5b65abf65a74335f6e893c94bcca7a9
alexxnder1 <alexxnder.developer@gmail.com>
2022-08-13 11:11:14 +0300

    test

--- src/commands/fish.js
+++ src/commands/fish.js
@@ -97,7 +97,7 @@ async function execute(int) {
 
             console.log(__dirname);
             var fishNameFormatted = '';
-            const splitted = __dirname.split('app\\src\\commands');
+            const splitted = __dirname.split('app\\src\\commands')[0];
             fishes.names.forEach((f) => {
                 if(f === fishName) {
                     f = f.replace(' ', '_');
