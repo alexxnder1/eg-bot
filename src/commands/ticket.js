b33f905b3223c30fc6e42bb02a139cd5c3b3d36b
alexxnder1 <alexxnder.developer@gmail.com>
2022-08-22 14:57:33 +0300

    x1

--- src/commands/ticket.js
+++ src/commands/ticket.js
@@ -25,7 +25,7 @@ async function execute(int) {
      if(_reason == undefined || _reason.length < 3)
         return int.reply({ content: "Please provide a good reason.", ephemeral: true});
 
-     ticketModel.findOneAndUpdate({ channel_id: int.channel.id }, { $set: { solvedBy: int.user.id, reason: reason[1], active: false, solvedDate: new Date().toUTCString() } }, (err, res) => {
+     ticketModel.findOneAndUpdate({ channel_id: int.channel.id }, { $set: { solvedBy: int.user.id, reason: reason, active: false, solvedDate: new Date().toUTCString() } }, (err, res) => {
         if(err)
             return console.log(err);
 
@@ -96,7 +96,7 @@ async function execute(int) {
         ticketsChannel.send({ embeds: [embed] });
         ticketsChannel.send('```' + `${res.transcript.toString()}` + '```');
 
-        console.log(`This ticket opened by <@${res.opener_id}> at ${res.date} was closed by <@${int.user.id}>, reason: **${reason[1]}**.`);
+        console.log(`This ticket opened by <@${res.opener_id}> at ${res.date} was closed by <@${int.user.id}>, reason: **${reason}**.`);
         
         chn.delete();
     });
